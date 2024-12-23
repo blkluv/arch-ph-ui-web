7b7b8cbfcc1b19809d1a4a43d935a5d1211c76bc
francispena <francis.gail.pena@gmail.com>
2024-12-23 17:06:12 -0500

    init code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # arch-ph-ui-web
+
 Arch PH Web UI dental application
+
+install tailwind
+npm install tailwindcss
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -9,11 +9,11 @@ import {
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
-    href: '/dashboard/invoices',
-    icon: DocumentDuplicateIcon,
+    name: 'Patients',
+    href: '/patient',
+    icon: UserGroupIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'Reports', href: '/reports', icon: DocumentDuplicateIcon },
 ];
 
 export default function NavLinks() {
@@ -22,7 +22,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <a  
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
